26b4bca98de29dd4c9e5b01815db674c51d49b1f
aveybd <debashishdab@gmail.com>
2019-04-09 23:56:35 +0600

    Initial commit

---
+++ regsv.php
@@ -0,0 +1,56 @@
+<?php
+include('includes/config.php');
+if(isset($_POST['submit']))
+{
+
+$file = $_FILES['image']['name'];
+$file_loc = $_FILES['image']['tmp_name'];
+$folder="images/";
+$new_file_name = strtolower($file);
+$final_file=str_replace(' ','-',$new_file_name);
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$password=md5($_POST['password']);
+$gender=$_POST['gender'];
+$mobileno=$_POST['mobileno'];
+$designation=$_POST['designation'];
+
+if(move_uploaded_file($file_loc,$folder.$final_file))
+	{
+		$image=$final_file;
+    }
+$notitype='Create Account';
+$reciver='Admin';
+$sender=$email;
+
+$sqlnoti="insert into notification (notiuser,notireciver,notitype) values (:notiuser,:notireciver,:notitype)";
+$querynoti = $dbh->prepare($sqlnoti);
+$querynoti-> bindParam(':notiuser', $sender, PDO::PARAM_STR);
+$querynoti-> bindParam(':notireciver',$reciver, PDO::PARAM_STR);
+$querynoti-> bindParam(':notitype', $notitype, PDO::PARAM_STR);
+$querynoti->execute();
+
+$sql ="INSERT INTO users(name,email, password, gender, mobile, designation, image, status) VALUES(:name, :email, :password, :gender, :mobileno, :designation, :image, 0)";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':name', $name, PDO::PARAM_STR);
+$query-> bindParam(':email', $email, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> bindParam(':gender', $gender, PDO::PARAM_STR);
+$query-> bindParam(':mobileno', $mobileno, PDO::PARAM_STR);
+$query-> bindParam(':designation', $designation, PDO::PARAM_STR);
+$query-> bindParam(':image', $image, PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+echo "<script type='text/javascript'>alert('Registration Sucessfull!');</script>";
+echo "<script type='text/javascript'> document.location = 'index.php'; </script>";
+}
+else
+{
+$error="Something went wrong. Please try again";
+}
+
+}
+?>
